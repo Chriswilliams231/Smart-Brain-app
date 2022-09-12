96833695d028bcf252b724e2552a9d198d993374
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-09-12 16:22:47 -0700

    adding the steps to the facial Api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,52 @@
 import React, { Component } from 'react';
-
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: '20f8d819af4a40bf8b4c78573f567ca3'
+});
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    console.log("click")
+    app.models.perdict(
+      Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(
+        function (response) {
+          console.log(response)
+        },
+        function (error) {
+
+        }
+      )
+  }
   render() {
     return (
       <div className="App">
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition />  */}
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
