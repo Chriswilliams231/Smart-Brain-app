393e29b7f5234961a082b082a563e3210f236b92
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-09-13 15:00:05 -0700

    tried to downgrade the CRA but it didnt work. trying to work around it

--- src/App.js
+++ src/App.js
@@ -8,17 +8,35 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 
 import './App.css';
 
-const app = new Clarifai.App({
-  apiKey: '20f8d819af4a40bf8b4c78573f567ca3'
-});
+// const app = new Clarifai.App({
+//   apiKey: "20f8d819af4a40bf8b4c78573f567ca3"
+// });
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
-      imageUrl: ''
+      imageUrl: '',
+      box: {},
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box })
   }
 
   onInputChange = (event) => {
@@ -28,16 +46,12 @@ class App extends Component {
   onButtonSubmit = () => {
     this.setState({ imageUrl: this.state.input })
     console.log("click")
-    app.models.perdict(
-      Clarifai.FACE_DETECT_MODEL, this.state.input)
-      .then(
-        function (response) {
-          console.log(response)
-        },
-        function (error) {
+    // app.models
+    //   .predict(
+    //     '53e1df302c079b3db8a0a36033ed2d15', this.state.input)
+    //   .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    //   .catch(err => console.log(err))
 
-        }
-      )
   }
   render() {
     return (
@@ -46,7 +60,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
