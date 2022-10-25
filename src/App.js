c3ee6ce7bdb1ac0931ba897aef2648022a42c681
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-10-25 15:07:12 -0700

    deploy

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
 
   onPictureSubmit = () => {
     this.setState({ imageUrl: this.state.input })
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://dry-citadel-36680.herokuapp.com/imageurl', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
@@ -85,7 +85,7 @@ class App extends Component {
       .then(response => {
         console.log(response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://dry-citadel-36680.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
